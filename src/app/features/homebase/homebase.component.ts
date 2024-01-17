095b529a50b684e7c8a32677221eee08705fd417
802023 <136094955+devwebsoluctions@users.noreply.github.com>
2024-01-17 07:30:46 -0300

    Ajustando chamadas

--- src/app/features/homebase/homebase.component.ts
+++ src/app/features/homebase/homebase.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProdutosService } from 'src/app/shared/services/produtos.service';
 
 @Component({
   selector: 'app-homebase',
   templateUrl: './homebase.component.html',
   styleUrls: ['./homebase.component.scss']
 })
-export class HomebaseComponent {
+export class HomebaseComponent implements OnInit{
+
+  constructor(
+    private produtosService: ProdutosService
+  ) {}
+
+  ngOnInit(): void {
+
+
+      console.log('teste')
+
+
+  }
 
 }
+
+
