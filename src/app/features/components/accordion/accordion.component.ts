23655006202c6c781ed4649c4ee95332c757493f
802023 <136094955+devwebsoluctions@users.noreply.github.com>
2024-01-26 06:25:14 -0300

    Limpando Estrutura para poder consumir do zero

--- src/app/features/components/accordion/accordion.component.ts
+++ src/app/features/components/accordion/accordion.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Iprodutos } from 'src/app/shared/models/Iprodutos';
-import { GetProdCategoriasService } from 'src/app/shared/services/utils/get-prod-categorias.service';
 
 @Component({
   selector: 'app-accordion',
@@ -8,16 +6,11 @@ import { GetProdCategoriasService } from 'src/app/shared/services/utils/get-prod
   styleUrls: ['./accordion.component.scss']
 })
 export class AccordionComponent implements OnInit {
-  public categoriasComProdutos: any = {}
 
   constructor(
-    private getProdCategoriasService : GetProdCategoriasService
   ) {}
 
   ngOnInit(): void {
-    this.getProdCategoriasService.obterCategorias().then(() => {
-      this.categoriasComProdutos = this.getProdCategoriasService.categoriasFiltradoProduto;
-      console.log(this.categoriasComProdutos);
-    });
+
   }
 }
--- src/app/shared/services/utils/get-prod-categorias.service.ts
+++ src/app/shared/services/utils/get-prod-categorias.service.ts
@@ -1,52 +1,10 @@
 import { Injectable } from '@angular/core';
 
-import { ProdutosService } from 'src/app/shared/services/produtos.service';
-
-//INTERFACE
-import { results } from 'src/app/shared/models/Iprodutos';
-
 @Injectable({
   providedIn: 'root'
 })
 export class GetProdCategoriasService {
 
-  categorias: results[] = [];
-  produtos: results[] = [];
+  constructor() {}
 
-  categoriasFiltradoProduto: any = {};
-
-  constructor(
-    private produtosService: ProdutosService
-  ) {}
-
-  obterCategorias(): any {
-    // Obter categorias
-    this.produtosService.getCategorias().then(response => {
-      this.categorias = response?.results || [];
-      this.obterProdutos();
-    });
   }
-
-  obterProdutos(): any {
-    // Obter produtos
-    this.produtosService.getProdutos().then(response => {
-      this.produtos = response?.results || [];
-      this.associarProdutosACategorias();
-    });
-  }
-
-  associarProdutosACategorias(): any {
-    // Estruturar os dados
-    this.categorias.forEach(categoria => {
-      if (categoria && categoria.id !== undefined) {
-      this.categoriasFiltradoProduto[categoria.id] = {
-        ...categoria,
-        produtos: this.produtos.filter(produto => produto.categoria_id === categoria.id)
-      };
-    }
-    });
-
-    // console.log(this.categoriasFiltradoProduto);
-    return this.categoriasFiltradoProduto
-  }
-}
